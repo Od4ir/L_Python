ca150bf66bf1598c99ab7197e04a7ecef8fa963d
Od4ir <101140991+Od4ir@users.noreply.github.com>
2022-09-26 17:29:17 +0000

    E5, E6, E7 e inputs

---
+++ E6.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um número: '))
+i = int(input('Digite o índice da raiz: '))
+
+raiz = n ** (1/i)
+
+print("A raiz de índice {} de {} é {}".format(i, n, raiz))
---
+++ input1.py
@@ -0,0 +1,5 @@
+resposta = input("Digite alguma coisa: ")
+print(type(resposta))
+
+
+#Toda entrada é lida como uma string (str);
